0ce7346e6f00108b5ebf85ca36dd4d92729b4cbf
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-18 21:53:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "arrays!"
+  array << "wow"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift("arrays!")
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
